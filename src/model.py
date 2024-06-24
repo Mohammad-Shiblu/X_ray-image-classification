1781961ea7443dfcc6dd1845e73bb997e1d9c4b3
Shiblu <mohammad.shiblu@fau.de>
2024-06-25 00:58:45 +0200

    convolution model is added

---
+++ src/model.py
@@ -0,0 +1,19 @@
+
+from tensorflow.keras.model import Sequential
+from tesnorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+def build_model(input_shape=(224, 224, 3)):
+    model = Sequential([
+        Conv2D(32, (3, 3), activation = 'relu', input_shape = input_shape),
+        MaxPooling2D((2, 2,)),
+        Conv2D(64, (3,3), activation = 'relu'),
+        MaxPooling2D((2,2)),
+        Conv2D(128, (3,3,), activation='relu'),
+        MaxPooling2D((2,2)),
+        Flatten(),
+        Dense(256, activation = 'relu'),
+        Dropout(0.5),
+        Dense(1, activation = 'sigmoid')
+    ])
+
+    return model
